d553f12eea4caa7679825b39e7f2ad380004e238
ryu820 <rhju820@naver.com>
2024-03-03 23:50:43 +0900

    [FEAT]Redis connect set

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const dotenv = require('dotenv')
+const redis = require('redis');
+dotenv.config()
+const env = process.env;
 
+const redisClient = redis.createClient({ 
+    legacyMode: true 
+})
+redisClient.on('connect', () => {
+    console.info('Redis Connect~')
+})
+redisClient.on('error', () => {
+    console.error('Redis Connect Error', error)
+})
+redisClient.connect().then()
+const redisCli = redisClient.v4
 
 router.get("/health", (req, res) => {
     res.status(200).json("health Check")
 })
+
 
 
 
